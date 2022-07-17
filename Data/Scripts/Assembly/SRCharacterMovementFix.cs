ad5506a7f310b3a1a1c99c59944f7a9cf8922f28
paradoxReborn <96443128+paradoxReborn@users.noreply.github.com>
2022-07-17 12:06:42 -0500

    Improved performance and reliability.

--- Data/Scripts/Assembly/SRCharacterMovementFix.cs
+++ Data/Scripts/Assembly/SRCharacterMovementFix.cs
@@ -1,20 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VRage.Game.Components;
 using VRage.Game.ModAPI;
 using VRage.ModAPI;
 using Sandbox.ModAPI;
 using Sandbox.Game;
 using Sandbox.Game.Entities;
-using Sandbox.Game.Components;
-using VRage.ObjectBuilders;
-using VRage.Game;
 using VRage.Utils;
-using Sandbox.Game.EntityComponents;
-using Sandbox.Game.Entities.Character.Components;
 
 namespace SurvivalReborn
 {
@@ -24,9 +16,8 @@ namespace SurvivalReborn
     /// - Lower fall/collision damage threshold
     /// - Smooth out player movement a bit
     /// After playing with these settings, vanilla may feel a bit "jerky" or jarring.
-    /// Defaults are restored on world close.
-    /// 
-    /// TODO: I want to add "recoil" on damage both from impacts and attacks.
+    /// Defaults are restored on world close in case the mod is removed.
+    /// This does not save any data to the game's save file.
     /// </summary>
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
     class SRCharacterMovementFix : MySessionComponentBase
@@ -36,29 +27,44 @@ namespace SurvivalReborn
         {
             public SRCharacterInfo(IMyCharacter character)
             {
+                Character = character;
                 LastKnownParent = character.Parent;
-                CollisionDamageDisabled = true; // disabled until character moves to prevent damage on world load on moving ship
+                CollisionDamageEnabled = false; // disabled until character moves to prevent damage on world load on moving ship
                 CollisionDisabledForFrames = 2; // disabled for two frames to prevent damage on respawn on moving ship
             }
 
+            public IMyCharacter Character;
             public IMyEntity LastKnownParent;
-            public bool CollisionDamageDisabled;
+            public bool CollisionDamageEnabled;
             public int CollisionDisabledForFrames;
         }
 
         // List of characters to apply fall damage game rule to
-        Dictionary<IMyCharacter, SRCharacterInfo> charactersInWorld = new Dictionary<IMyCharacter,SRCharacterInfo>();
-        List<IMyCharacter> charactersToUpdate = new List<IMyCharacter>();
+        List<SRCharacterInfo> m_characterInfos = new List<SRCharacterInfo>();
 
-        // Game rules for fall damage - settings should be in m/s/s
+        // Game rules for fall damage - settings are in m/s/s
         const float DAMAGE_THRESHOLD = 750f;
         const float IGNORE_ABOVE = 1500f; // Should be roughly where vanilla damage starts
         const float DAMAGE_PER_MSS = 0.04f;
+
+        // Defaults to restore
+        private float m_defaultCharacterGravity;
+        private float m_defaultWalkAcceleration;
+        private float m_defaultWalkDeceleration;
+        private float m_defaultSprintAcceleration;
+        private float m_defaultSprintDeceleration;
 
         public override void LoadData()
         {
             // Hook entity add for character list
             MyEntities.OnEntityAdd += MyEntities_OnEntityAdd;
+
+            // Remember defaults
+            m_defaultCharacterGravity = MyPerGameSettings.CharacterGravityMultiplier;
+            m_defaultWalkAcceleration = MyPerGameSettings.CharacterMovement.WalkAcceleration;
+            m_defaultWalkDeceleration = MyPerGameSettings.CharacterMovement.WalkDecceleration;
+            m_defaultSprintAcceleration = MyPerGameSettings.CharacterMovement.SprintAcceleration;
+            m_defaultSprintDeceleration = MyPerGameSettings.CharacterMovement.SprintDecceleration;
 
             // Fix supergravity
             MyPerGameSettings.CharacterGravityMultiplier = 1f;
@@ -81,12 +87,12 @@ namespace SurvivalReborn
             // Unhook events
             MyEntities.OnEntityAdd -= MyEntities_OnEntityAdd;
 
-            // Restore defaults
-            MyPerGameSettings.CharacterGravityMultiplier = 2f;
-            MyPerGameSettings.CharacterMovement.WalkAcceleration = 50f;
-            MyPerGameSettings.CharacterMovement.WalkDecceleration = 10f;
-            MyPerGameSettings.CharacterMovement.SprintAcceleration = 100f;
-            MyPerGameSettings.CharacterMovement.SprintDecceleration = 20f;
+            // Restore all defaults
+            MyPerGameSettings.CharacterGravityMultiplier = m_defaultCharacterGravity;
+            MyPerGameSettings.CharacterMovement.WalkAcceleration = m_defaultWalkAcceleration;
+            MyPerGameSettings.CharacterMovement.WalkDecceleration = m_defaultWalkDeceleration;
+            MyPerGameSettings.CharacterMovement.SprintAcceleration = m_defaultSprintAcceleration;
+            MyPerGameSettings.CharacterMovement.SprintDecceleration = m_defaultSprintDeceleration;
 
             //Log
             MyLog.Default.WriteLine("SurvivalReborn: MyPerGameSettings returned to defaults.");
@@ -95,84 +101,90 @@ namespace SurvivalReborn
         private void MyEntities_OnEntityAdd(VRage.Game.Entity.MyEntity obj)
         {
             IMyCharacter character = obj as IMyCharacter;
-            if (character != null && !charactersInWorld.ContainsKey(character)) // This could be inefficient
+            if (character != null)
             {
-                //Add to list
-                charactersInWorld.Add(character, new SRCharacterInfo(character));
-                //charsInWorld.Add(new MyCharacterWrapper(character));
+                // Add to list
+                m_characterInfos.Add(new SRCharacterInfo(character));
+                // Prepare to remove character from list when it's removed from world
                 character.OnMarkForClose += Character_OnMarkForClose;
             }
         }
 
+        // Remove character from list when it's removed from the world.
         private void Character_OnMarkForClose(IMyEntity obj)
         {
-            //Remove from list
+            // Ensure this is a character. Ignore otherwise.
             IMyCharacter character = obj as IMyCharacter;
             if (character != null)
             {
-                charactersInWorld.Remove(character);
-                //charsInWorld.Remove(character);
+                // Remove character and log error if it's not present
+                if (!RemoveCharacterFromInfos(character))
+                    MyLog.Default.Error("SurvivalReborn: Attempted to remove a nonexistent character from characterInfos, or removal failed.");
+
                 character.OnMarkForClose -= Character_OnMarkForClose;
             }
         }
 
+        // Remove character from infos if present and return true. Return false on failure.
+        // This isn't terribly efficient, but only needs to execute once per character and SE doesn't support huge player counts.
+        private bool RemoveCharacterFromInfos(IMyCharacter character)
+        {
+            foreach (SRCharacterInfo info in m_characterInfos)
+            {
+                if (info.Character.Equals(character))
+                {
+                    m_characterInfos.Remove(info);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
         public override void UpdateBeforeSimulation()
         {
             base.UpdateBeforeSimulation();
-            //if (MyAPIGateway.Session.Player.Character.Physics.LinearAcceleration.Length() > 500f) MyAPIGateway.Utilities.ShowNotification("OOF");
-            foreach (KeyValuePair<IMyCharacter, SRCharacterInfo> entry in charactersInWorld)
+            foreach (SRCharacterInfo info in m_characterInfos)
             {
-                IMyCharacter character = entry.Key;
-                SRCharacterInfo info = entry.Value;
-
-                if (info.CollisionDamageDisabled)
+                // Skip updating this character until it moves. This ensures phsyics have been fully loaded.
+                if (!info.CollisionDamageEnabled)
                 {
-                    if(character.Physics.LinearAcceleration.Length() > 0)
+                    if (info.Character.Physics.LinearAcceleration.Length() > 0)
                     {
-                        info.CollisionDamageDisabled = false;
+                        info.CollisionDamageEnabled = true;
                     }
                     continue;
                 }
 
                 // If parent changed this frame, ignore collision damage and update last known parent.
-                if (info.LastKnownParent != character.Parent)
+                if (info.LastKnownParent != info.Character.Parent)
                 {
-                    charactersToUpdate.Add(character);
+                    // Update parent and disable collision damage for the next frame.
+                    // (Damage would normally occur on the following frame)
+                    info.LastKnownParent = info.Character.Parent;
+                    info.CollisionDisabledForFrames = 2;
+                    MyLog.Default.WriteLine("SurvivalReborn: Character parent changed to " + info.Character.Parent + ". collision damage disabled for " + info.CollisionDisabledForFrames + " frames.");
+                    //MyAPIGateway.Utilities.ShowNotification("Character parent changed to " + character.Parent);
                 }
                 // If parent didn't change this frame, go ahead with possible collision damage unless it's disabled
-                //else if (!info.CollisionDamageDisabled)
                 else if (info.CollisionDisabledForFrames <= 0)
                 {
-                    float accel = character.Physics.LinearAcceleration.Length();
+                    float accel = info.Character.Physics.LinearAcceleration.Length();
                     if (accel > DAMAGE_THRESHOLD)
                     {
                         float damage = Math.Min(IGNORE_ABOVE * DAMAGE_PER_MSS, DAMAGE_PER_MSS * (accel - DAMAGE_THRESHOLD));
-                        character.DoDamage(damage, MyStringHash.GetOrCompute("Environment"), true);
-                        MyLog.Default.WriteLine("Did collision damage for " + accel + " m/s/s");
-                        MyAPIGateway.Utilities.ShowNotification("OOF! " + accel + " m/s/s", 5000, "Red");
+                        info.Character.DoDamage(damage, MyStringHash.GetOrCompute("Environment"), true);
+                        MyLog.Default.WriteLine("SurvivalReborn: Did collision damage for " + accel + " m/s/s");
+                        //MyAPIGateway.Utilities.ShowNotification("OOF! " + accel + " m/s/s", 5000, "Red");
                     }
                 }
+                // If collision damage is still disabled for some number of frames, decrement that number.
                 else
                 {
-                    //info.CollisionDamageDisabled = false;
                     info.CollisionDisabledForFrames--;
                 }
             }
-
-            // Can't modify charactersInWorld while enumerating through it, so do it here.
-            foreach (IMyCharacter character in charactersToUpdate)
-            {
-                var info = charactersInWorld[character];
-
-                // Update parent and disable collision damage for the next frame.
-                // (Damage would normally occur on the following frame)
-                info.LastKnownParent = character.Parent;
-                //info.CollisionDamageDisabled = true;
-                info.CollisionDisabledForFrames = 2;
-                MyAPIGateway.Utilities.ShowNotification("Character parent changed to " + character.Parent);
-            }
-            charactersToUpdate.Clear();
         }
     }
-    
+
 }
