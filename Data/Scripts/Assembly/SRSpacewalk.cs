5feaaaa71a396168c875b30484ee57334243aa7d
paradoxReborn <96443128+paradoxReborn@users.noreply.github.com>
2022-07-23 21:30:11 -0500

    Added GPL notice

--- Data/Scripts/Assembly/SRSpacewalk.cs
+++ Data/Scripts/Assembly/SRSpacewalk.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿///    Copyright (C) 2022 Matthew Kern, a.k.a. Paradox Reborn
+///
+///This program is free software: you can redistribute it and/or modify
+///    it under the terms of the GNU General Public License as published by
+///    the Free Software Foundation, either version 3 of the License, or
+///    (at your option) any later version.
+///
+///    This program is distributed in the hope that it will be useful,
+///but WITHOUT ANY WARRANTY; without even the implied warranty of
+///    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+///    GNU General Public License for more details.
+///
+///    You should have received a copy of the GNU General Public License
+///    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+///
+///    In accordance with section 7 of the GNU General Public License,
+///    the license is supplementd with the following additional terms:
+///    1. You may not claim affiliation with Survival Reborn or its author.
+///    2. You must not represent your work as being part of the Survival Reborn series 
+///    or use the Survival Reborn name or imagery in any misleading or deceptive way.
+
+using System;
 using System.Collections.Generic;
 using VRage.Game;
 using VRage.Game.Entity;
@@ -31,8 +52,8 @@ namespace SurvivalReborn
     /// - Smooth out player movement a bit
     /// Defaults are restored on world close in case the mod is removed. Otherwise the world would keep the modded global values.
     /// 
-    /// TODO/OPTIMIZATION: Change collision damage checks each tick to compare acceleration LengthSquared to the square of the threshold.
-    ///     When the threshold is passed, only then should I call .Length() which includes a square root.
+    /// KNOWN ISSUES:
+    /// - The jetpack fuel level visually desyncs in multiplayer when the game tries (in vain) to refuel it.
     /// 
     /// Jetpack anti-refueling works regardless of the gas a specific character uses for fuel.
     /// </summary>
